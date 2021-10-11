cdbb196c25332c10b164a1eb8936ee613af8b529
anas-abusaif <anasabusief@gmail.com>
2021-10-11 15:29:34 +0300

    updated challange 1

--- python/array-reverse.py
+++ python/array-reverse.py
@@ -2,7 +2,11 @@ nums=[1,2,3,4,5,6,7,8,9]
 
 
 def array_reverse(list):
-  return nums[::-1]
-
+ x=len(list)-1
+ new_list=[]
+ for l in list:
+   new_list.append(list[x])
+   x-=1
+ return new_list
 
 print(array_reverse(nums))
