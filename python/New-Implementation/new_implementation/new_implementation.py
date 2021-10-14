5fe04c9ee69d5bee353264c71e6631fd0db1c147
anas-abusaif <anasabusief@gmail.com>
2021-10-14 22:33:39 +0300

    in progress

---
+++ python/New-Implementation/new_implementation/new_implementation.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, next_, data_):
+        self.data_ = data_
+        self.next_ = next_
+
+
+class Linked_list:
+    def __init__(self, head=None):
+        self.head = head
+
+
+def insert(self, value):
+    self.head = Node(value, self.head)
+
+
+def includes(self, value):
+    pass
+
+
+def to_string(self):
+    pass
