a5ea5eaeee2b720d3562c4ffc1536fee3f964c90
anas-abusaif <anasabusief@gmail.com>
2021-11-01 20:21:27 +0200

    challange 17

--- python/trees/trees/breadth_first.py
+++ python/trees/trees/breadth_first.py
@@ -1,23 +1,20 @@
-def breadth_first(tree_):
-    breadth = []
-    breadth+=tree_.root
+from trees.trees import Queue
+
+def breadth_first(tree):
+    
+    breadth = Queue()
+    breadth.enqueue(tree.root)
+
     list_of_items = []
 
-    while breadth[0]:
-      front = breadth[0]
-      for i in breadth:
-        arr=[]
-        if i==front:
-          continue
-        arr+=i
-        breadth=arr
-
+    while breadth.peek():
+      front = breadth.dequeue()
       list_of_items += [front.data]
 
       if front.left:
-        breadth+=[front.left]
+        breadth.enqueue(front.left)
 
       if front.right:
-        breadth+=[front.right]
+        breadth.enqueue(front.right)
 
     return list_of_items
